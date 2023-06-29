448440a495a0e81edc20dcedc79d469929659b17
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-06-29 15:30:12 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,7 @@
-'use strict'
+'use strict';
 
-// && - and, 그리고, 이면서 : 둘 다 참이면 참
-console.log(false && false); 
-console.log(true && false);
-console.log(false && true);
-console.log(true && true);
-console.log();
+let number;
 
-// || - or, 또는, 이거나 : 둘 중 하나만 참이어도 참
-console.log(false || false);
-console.log(true || false);
-console.log(false || true);
-console.log(true || true);
+number = prompt('1 ~ 100 사이의 자연수를 입력 :');
+
+alert(number >= 2 && number <= 99 ? '정확히 입력!!' : '입력오류');

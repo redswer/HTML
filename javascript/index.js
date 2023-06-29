ce631804933a553bb6c18c27b164cedeae0375a8
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-06-29 14:45:30 +0900

    삼항조건 연산자

--- javascript/index.js
+++ javascript/index.js
@@ -1,37 +1,21 @@
-/* 내장(built-in) 메서드 */
-
-/* 형식 : [window.]메서드(); */
-
-/* ==================================================== */
-
-/* prompt - 브라우저 입력 대화상자 창 활성화 */
-
-/* 형식 : prompt( message? : string, default? : string) : string
-
-    ?   : 생략 가능함을 의미
-message : 입력 대화상자에 표시할 문자열 메세지 지정
-default : 입력 대화상자 입력란에 기본적으로 입력되어질 문자열 지정
-
-- 미 입력 후 확인 버튼을 누르면 빈 문자열('') 반환
-- 취소 버튼을 누르면 null 값 반환 */
-
-/* ===================================================== */
-
-/* alert - 브라우저 경고 대화상자 창 활성화 */
-
-/* 형식 : alert( message? : any ) : void */
-
-/* 무조건 입력 / 추후 자세한 설명 */
 'use strict';
 
-let input;
+/* 삼항조건 연산자 */
 
-input = prompt();
-alert(input);
+/* 형식 : 조건식 ? 참실행 : 거짓실행 */
 
+// javascript 에서는 false를 0, true 를 0이 아닌 다른 값으로 표현할 수 있음 / java 에서는 불가능
+// true(1) 면 남성/남자, false(0) 면 여성/여자
+let gender = 1;
 
-input = prompt('입력');
-alert(input);
+console.log(`당신의 성별은 ${gender ? '남성' : '여성'}입니다.`);
 
-input = prompt('입력', '기본데이터');
-alert(input)
+let genderText;
+
+// javascript 에서는 좌변에 조건식을 쓸 수 있음 / java 에서는 불가능
+gender ? genderText = '남자' : genderText = '여자';
+
+console.log(genderText);
+
+// javascript 에서만 다중 조건식가능
+// 조건식1 ? 참 : 조건식 ? 참 : 거짓;

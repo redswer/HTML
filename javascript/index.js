15cb11e82521bcfc8bbb5918aa38dfc63cc0fd22
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-06-29 15:00:39 +0900

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,14 @@
-'use strict';
+'use strict'
 
-/* 삼항조건 연산자 */
+// && - and, 그리고, 이면서 : 둘 다 참이면 참
+console.log(false && false); 
+console.log(true && false);
+console.log(false && true);
+console.log(true && true);
+console.log();
 
-/* 형식 : 조건식 ? 참실행 : 거짓실행 */
-
-// javascript 에서는 false를 0, true 를 0이 아닌 다른 값으로 표현할 수 있음 / java 에서는 불가능
-// true(1) 면 남성/남자, false(0) 면 여성/여자
-let gender = 1;
-
-console.log(`당신의 성별은 ${gender ? '남성' : '여성'}입니다.`);
-
-let genderText;
-
-// javascript 에서는 좌변에 조건식을 쓸 수 있음 / java 에서는 불가능
-gender ? genderText = '남자' : genderText = '여자';
-
-console.log(genderText);
-
-// javascript 에서만 다중 조건식가능
-// 조건식1 ? 참 : 조건식 ? 참 : 거짓;
+// || - or, 또는, 이거나 : 둘 중 하나만 참이어도 참
+console.log(false || false);
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);

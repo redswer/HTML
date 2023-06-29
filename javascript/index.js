6b04dfacf3b5af943ebb2a929b0a734b394900a0
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-06-29 16:36:46 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,17 @@
 'use strict';
 
-let number;
+let number1, number2;
 
-number = prompt('1 ~ 100 사이의 자연수를 입력 :');
 
-alert(number >= 2 && number <= 99 ? '정확히 입력!!' : '입력오류');
+/* 문자열 '1' 과 숫자 1 을 더하면 문자열 11이 출력됨
+   따라서 문자열 '1' 을 숫자로 변환하려면 *1 을 해줘야 함 = prompt 앞에 +를 붙이는 것과 같은 효과 */
+number1 = +prompt('첫번째 정수 입력 : ');
+
+number2 = +prompt('두번째 정수 입력 : ');
+
+// alert(number1 > number2 ? '큰 수는 ' + number1 :  number1 < number2 ?  '큰 수는 ' + number2 : '같음');
+
+alert(number1 == number2 ? '같음' : `큰 수는 ${number1 < number2 ? number2 : number1}`);
+/* alert(number1 == number2 ? '같음 : '큰 수는' + number1 < number2 ? number2 :number1); 에서는
+    '큰 수는'과 number1 < number2 ? 이 먼저 결합하므로 + 다음을 ()로 묶어줘야 함
+    따라서 alert(number1 == number2 ? '같음 : '큰 수는' + (number1 < number2 ? number2 :number1)); 가 됨 */

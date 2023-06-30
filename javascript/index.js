fd9fa0a9041785cac5226106d4f87ed247049538
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-06-30 18:06:03 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,23 @@
 'use strict'
 
 let tot = 0;
-let i = 0;
+let i = 1;
 
-while (i < 100) {
-    i = i + 1;
-    tot = tot + i;
-}
+do {
+    tot += i;           // tot = tot + i;
+    i++;                // i = i + 1;
+} while (i < 101)
 
-console.log(tot);
+console.log(tot)
 
 /* 
 최초가상과거누적합 = 0;
-과거대상값 = 0;
+과거대상값 = 1;
 
-while (현재대상값 < 100) {
-    현재대상값 = 과거대상값 + 1;
+do {
     현재누적합 = 과거누적합 + 현재대상값;
-}
+    현재대상값 = 과거대상값 + 1;
+} while (i < 101);
 
 console.log(현재누적합);
 */

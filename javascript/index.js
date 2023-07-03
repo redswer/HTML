1e25bde37b43132e45d7617f9619e7001ea36f00
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-03 10:42:43 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,29 @@
 'user strict';
 
 /* 
-작은 수 : 1
-큰 수 : 10
+정수1 : 1
+정수2 : 10
 
 1부터 10 까지 누적합 : 55
+
+정수1 : 100
+정수2 : 1
+
+1부터 100까지 누적합 : 5050
 */
 
 let tot = 0,
+    n1, n2,
     min, max;
 
-min = +prompt('작은 수 : ');
-max = +prompt('큰 수 : ');
+n1 = +prompt('정수1 : ');
+n2 = +prompt('정수2 : ');
+
+if (n1 < n2) {
+    min = n1, max = n2;    
+} else {
+    min = n2, max = n1;
+}
 
 /* for (let i = min; i < max + 1; i++) {
     tot = tot + i;
@@ -21,4 +33,4 @@ for (let i = min; i <= max; i++) {
     tot += i;
 }
 
-alert(`${min} 부터 ${max} 까지 누적합 : ${tot}`)
+alert(`${min} 부터 ${max} 까지 누적합 : ${tot}`);

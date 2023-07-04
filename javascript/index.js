dede55515992c9c860e8230c437f91483ba93212
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 14:37:59 +0900

    예제(행, 열) 1

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,13 @@
 'use strict';
 
-/* v = 1; */
+// '예제(행, 열)' 엑셀문서 참고
 
-// 하나의 로직이 끝나고 나서도 사용되면 전역변수로 선언
 let v = 1;
 
-// 하나의 로직 안에서만 사용하면 지역변수로 선언
-for (let i = 1, v = 1; i <= 5; i++) {       // i = 행
-    // 안쪽의 중첩문에는 사용 x
-    for (let j = 1; j <= 5; j++) {          // j = 열
+for (let i = 1, v = 1, ed = 1; i <= 5; i++) {   // 열종료변수 ed 는 1부터 시작 ==>  let ed = 1;
+    for (let j = 1; j <= ed; j++) {     // 1행일때 ed = 1, 2행일때 2, ... 5행일때 5 ==> j <= ed;
         document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
     }
     document.write('<br/>');
+    ed++;
 }

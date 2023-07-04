d57c953f224697d96af6780705b92cfeae0dc414
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 12:40:48 +0900

    구구단(레이블)

--- javascript/index.js
+++ javascript/index.js
@@ -4,21 +4,19 @@
 
 //  for (초기식; 조건식; 증감식)
 
+// 레이블 (out) 을 밖에다 걸면 안쪽 for문 안의 break 의 적용 대상이 바깥쪽 for문으로 바뀜 따라서 1단의 *5 까지만 출력됨
+// 변수처럼 활용 가능 (반드시 out 일 필요 없음)
+
+out:
 for (let i = 1; i < 10; i++) {
-    // continue
-    /* for : 증감식 부분으로 점핑 */
-    /* while, do-while : 증감식이 없기 때문에 조건식으로 점핑 */
-    
+
     for (let j = 1, result; j < 10; j++) {
+        if (j >= 6) break out;
         result = i * j;
 
         // 정렬
         document.write(`${i} X ${j} =${result / 10 >= 1 ? '' : '&nbsp;'}${result} &nbsp; &nbsp;`);
-        // = document.write(`${i} X ${j} =${String(result).length >= 2 ? '' :'&nbsp;'}${result} `);
     }
     // 줄바꿈
     document.write('<br/>');
-
-    if (i >= 6) continue;
-    /* continue 코드는 밑의 코드를 무시하고 증감식으로 점프하는 코드인데 밑에 코드가 없으므로 의미 없는 코드 */
 }

89d964a8763b4739dffa9f78a20d7e800f0659cd
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 12:33:58 +0900

    구구단(continue) 2

--- javascript/index.js
+++ javascript/index.js
@@ -2,11 +2,13 @@
 
 // 구구단
 
+//  for (초기식; 조건식; 증감식)
+
 for (let i = 1; i < 10; i++) {
     // continue
     /* for : 증감식 부분으로 점핑 */
     /* while, do-while : 증감식이 없기 때문에 조건식으로 점핑 */
-    if (i >= 6) continue;
+    
     for (let j = 1, result; j < 10; j++) {
         result = i * j;
 
@@ -16,4 +18,7 @@ for (let i = 1; i < 10; i++) {
     }
     // 줄바꿈
     document.write('<br/>');
+
+    if (i >= 6) continue;
+    /* continue 코드는 밑의 코드를 무시하고 증감식으로 점프하는 코드인데 밑에 코드가 없으므로 의미 없는 코드 */
 }

bf6716dad1d1a2cc18d8e72b5a04028895152622
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 14:48:42 +0900

    예제(행, 열) 2

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
-// '예제(행, 열)' 엑셀문서 참고
+// '예제(행, 열) 2' 엑셀문서 참고
 
 let v = 1;
 
-for (let i = 1, v = 1, ed = 1; i <= 5; i++) {   // 열종료변수 ed 는 1부터 시작 ==>  let ed = 1;
-    for (let j = 1; j <= ed; j++) {     // 1행일때 ed = 1, 2행일때 2, ... 5행일때 5 ==> j <= ed;
+// 열종료변수는 행과 같으므로 변수 ed 를 사용해 표현하는 대신 행 값인 i 를 넣어주면 끝
+
+for (let i = 1, v = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
         document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
     }
     document.write('<br/>');
-    ed++;
 }

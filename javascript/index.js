4056c4303f39e4207702501634ab2cd3ef0b98b1
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 16:23:58 +0900

    예제(행, 열) 5

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
+// '예제(행, 열) 5' 엑셀파일 참고
+
 for (let i = 1, v = 1; i <= 5; i++) {
-    for (let j = 1; j <= 6 - i; j++) {
-        document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
+    for (let j = 1; j <= 5; j++) {
+        if (j < 6 - i) {
+            document.write('&nbsp; &nbsp;');
+        } else {
+            document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
+        }
     }
     document.write('<br/>');
 }

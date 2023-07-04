42629fcbda60e49d578e05612f0ddb498d9d2abf
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 12:15:44 +0900

    구구단(continue)

--- javascript/index.js
+++ javascript/index.js
@@ -3,17 +3,17 @@
 // 구구단
 
 for (let i = 1; i < 10; i++) {
-    // if (i >= 6) break;
+    // continue
+    /* for : 증감식 부분으로 점핑 */
+    /* while, do-while : 증감식이 없기 때문에 조건식으로 점핑 */
+    if (i >= 6) continue;
     for (let j = 1, result; j < 10; j++) {
-        if (j >= 6) break;
-// 모든 단에서 *6 까지만 출력됨
         result = i * j;
-        
+
         // 정렬
         document.write(`${i} X ${j} =${result / 10 >= 1 ? '' : '&nbsp;'}${result} &nbsp; &nbsp;`);
         // = document.write(`${i} X ${j} =${String(result).length >= 2 ? '' :'&nbsp;'}${result} `);
     }
     // 줄바꿈
     document.write('<br/>');
-    // if (i >= 6) break;
 }

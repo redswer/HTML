c5713b1f5ecc5a875e52a1078fae156c913f802c
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-04 12:14:52 +0900

    구구단(break) 2

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,7 @@
 // 구구단
 
 for (let i = 1; i < 10; i++) {
-    if (i >= 6) break;
-// 6 이상일때 빠져나오므로 5단까지만 출력됨
+    // if (i >= 6) break;
     for (let j = 1, result; j < 10; j++) {
         // if (j >= 6) break;
         result = i * j;
@@ -15,5 +14,6 @@ for (let i = 1; i < 10; i++) {
     }
     // 줄바꿈
     document.write('<br/>');
-    // if (i >= 6) break;
+    if (i >= 6) break;
+// 빠져나오기 전에 이미 위에서 6단까지 반복됬으므로 6단까지 출력됨
 }

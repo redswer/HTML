fcff3836d2f92cd56a4c80357c1d9fffa92c2ff6
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-05 10:39:03 +0900

    예제 (행, 열)  7 - 4자릿수 정렬

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,25 @@
 'use strict';
 
 let n;
+let numCnt;
 
 n = +prompt('줄수 : ');
 
-// 2자릿수 정렬
+// 4자릿수까지 정렬
+
+numCnt = String(n * n).length;
 
 for (let i = 1, v = 1; i <= n; i++) {
     for (let j = 1; j <= i + n - 1; j++) {
-        if (j < n - i + 1) {
-            document.write('&nbsp; &nbsp;');
+        if (j < n + 1 - i) {
+            for (let i = 0; i <= numCnt; i++) {
+                document.write('&nbsp;');
+            }
         } else {
-            document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
+            for (let i = 0; i < numCnt - String(v).length; i++) {
+                document.write('&nbsp;');
+            }
+            document.write(`${v++} `);
         }
     }
     document.write('<br/>');

9992d7648999936704308f9ef8488a67cad97863
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-05 16:59:37 +0900

    9

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,21 @@
 'use strict';
 
-// 8 참고
+for (let i = 1, v = 1, st, ed; i <= 5; i++) {
 
-for (let i = 1, v = 1; i <= 3; i++) {
-    for (let j = 1; j <= 6 - i; j++) {
-        if (j <= i - 1) {
-            document.write('&nbsp;&nbsp;');
+// let st = (i <= 2 ? i : 6 - i),
+//     ed = (i <= 2 ? 6 - i : i);
+
+if (i < 3) {
+    st = i, ed = 6 - i;
+} else {
+    st = 6 - i, ed = i;
+}
+
+    for (let j = 1; j <= ed; j++) {
+        if (j < st) {
+            document.write('&nbsp; &nbsp;');
         } else {
-            document.write(`${v++} `);
+            document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);
         }
     }
     document.write('<br/>');

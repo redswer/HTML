356115b4a31b8eef0e9c556e024d8f31945c655e
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-05 17:31:53 +0900

    10

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,18 @@
 'use strict';
 
-for (let i = 1, v = 1, st, ed; i <= 5; i++) {
+let n = +prompt('줄수 : ');
+let m = (n + 1) / 2
 
-// let st = (i <= 2 ? i : 6 - i),
-//     ed = (i <= 2 ? 6 - i : i);
+for (let i = 1, v = 1, st, ed; i <= n; i++) {
 
-if (i < 3) {
-    st = i, ed = 6 - i;
-} else {
-    st = 6 - i, ed = i;
-}
+    // let st = (i <= 2 ? i : 6 - i),
+    //     ed = (i <= 2 ? 6 - i : i);
+
+    if (i <= m) {
+        st = i, ed = n + 1 - i;
+    } else {
+        st = m * 2 - i, ed = i;
+    }
 
     for (let j = 1; j <= ed; j++) {
         if (j < st) {

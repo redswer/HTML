5e863e2c16d83d30264727a64cdf8c3f82e26747
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-05 10:38:25 +0900

    예제(행,열) 7 - 2자릿수 정렬

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
-// '예제(행, 열) 5' 엑셀파일 참고
+let n;
 
-for (let i = 1, v = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (j < 6 - i) {
+n = +prompt('줄수 : ');
+
+// 2자릿수 정렬
+
+for (let i = 1, v = 1; i <= n; i++) {
+    for (let j = 1; j <= i + n - 1; j++) {
+        if (j < n - i + 1) {
             document.write('&nbsp; &nbsp;');
         } else {
             document.write(`${String(v).length >= 2 ? '' : '&nbsp;'}${v++} `);

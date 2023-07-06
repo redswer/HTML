50451bf60214f4df0a5bc63c4a4cb197f6b4e72e
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-06 18:05:18 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,52 @@
 /*
     < 문제 >
 
-- 다음과 같이 email 변수에 이메일 주소가 저장되어 있을 때,
-  메일주소의 처음과 @ 앞에 "_" 를 삽입하고 @ 뒤의 도메인명은
-  개별 도메인명의 뒤에 ".kr"이 삽입되어 email 변수에 저장하도록
-  프로그램을 구현.
-  단, 아래 메일주소와 도메인명은 예시일뿐 다른 모든 메일주소와
-  도메인명에 대하여 적용되야함에 주의.
-
-    < 변환 예시 >
-
-    psy1234@daum.net       ->      _psy1234_@daum.kr.net
-
-    ysk999990@naver.com    ->      _ysk999990_@naver.kr.com
+- 주민번호를 입력받되 하이픈(-)을 포함하여 입력하면 "하이픈(-)을 공백으로 변경을 원하십니까?" 라는
+  문구를 표시하는 확인/취소 대화상자를 활성화시켜 "확인" 버튼을 클릭하면 공백으로 변경하고 "취소"
+  버튼을 클릭하면 하이픈이 포함된 그대로 대화상자에 표시되도록 프로그램 구현.
+  단, 하이픈이 포함되지 않은 경우에는 입력받은 그대로 대화상자에 표시되도록 구현.
 */
 
 'use strict';
 
-let email = 'psy1234@daum.net';
+/* 1. replace */
+let n = prompt('주민번호 : ');
 
-let atIdx = email.indexOf('@'),
-    dotIdx = email.indexOf('.');
+if (n.includes('-')) {
 
-email = `_${email.slice(0, atIdx)}_@${email.slice(atIdx + 1, dotIdx)}.kr${email.slice(dotIdx)}`;
+    if (confirm('하이픈(-)을 공백으로 변경을 원하십니까?')) {
+        n = n.replace('-', ' ');
+    }
+}
 
-// let atAr = email.split('@'),
-//     dotAr = atAr[1].split('.');
+alert(n);
 
-// email = `_${atAr[0]}_${dotAr[0]}.kr.${dotAr[1]}`
 
-console.log(email);
+// /* 2. slice */
+// let n = prompt('주민번호 : ');
+
+// if (n.includes('-')) {
+
+//     let hyphen = n.indexOf('-');
+
+//     if (confirm('하이픈(-)을 공백으로 변경을 원하십니까?')) {
+//         n = `${n.slice(0, hyphen)} ${n.slice(-(hyphen + 1))}`;
+//     }
+// }
+
+// alert(n);
+
+
+// /* 3. split */
+// let n = prompt('주민번호 : ');
+
+// if (n.includes('-')) {
+
+//     let hypen = n.split('-');
+
+//     if (confirm('하이픈(-)을 공백으로 변경을 원하십니까?')) {
+//         n = `${hypen[0]} ${hypen[1]}`;
+//     }
+// }
+
+// alert(n);

3c98a41eda8098ff73ff74324845ea221e172bb4
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-06 10:00:27 +0900

    11 - 2 (줄 입력받기)

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-for (let i = 1, v = 1, st, ed; i <= 5; i++) {
+let line, m;
 
-    if (i <= 2) {
-        st = 4 - i, ed = i + 2;
+line = +prompt('줄수 : ');
+m = (line + 1) / 2
+
+for (let i = 1, v = 1, st, ed; i <= line; i++) {
+
+    if (i <= m) {
+        st = m + 1 - i, ed = i + line - m;
     } else {
-        st = i - 2, ed = 8 - i;
+        st = i - (line - m), ed = line + m - i;
     }
 
     for (let j = 1; j <= ed; j++) {

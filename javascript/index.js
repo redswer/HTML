81e7a9cd25c93a9ba7e7e173f653cb976e6cac53
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-06 16:48:15 +0900

    예제

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,32 @@
+/*
+    < 문제 >
+
+- 다음과 같이 email 변수에 이메일 주소가 저장되어 있을 때,
+  메일주소의 처음과 @ 앞에 "_" 를 삽입하고 @ 뒤의 도메인명은
+  개별 도메인명의 뒤에 ".kr"이 삽입되어 email 변수에 저장하도록
+  프로그램을 구현.
+  단, 아래 메일주소와 도메인명은 예시일뿐 다른 모든 메일주소와
+  도메인명에 대하여 적용되야함에 주의.
+
+    < 변환 예시 >
+
+    psy1234@daum.net       ->      _psy1234_@daum.kr.net
+
+    ysk999990@naver.com    ->      _ysk999990_@naver.kr.com
+*/
+
 'use strict';
 
-let id = prompt('아이디 입력 : ', '영문자 숫자 조합으로만 입력');
+let email = 'psy1234@daum.net';
 
-alert(id.trim().toUpperCase());
+let atIdx = email.indexOf('@'),
+    dotIdx = email.indexOf('.');
+
+email = `_${email.slice(0, atIdx)}_@${email.slice(atIdx + 1, dotIdx)}.kr${email.slice(dotIdx)}`;
+
+// let atAr = email.split('@'),
+//     dotAr = atAr[1].split('.');
+
+// email = `_${atAr[0]}_${dotAr[0]}.kr.${dotAr[1]}`
+
+console.log(email);

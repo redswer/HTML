0a0775e505d98bd436c27063679790793284bb97
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-07 12:46:08 +0900

    래퍼 객체 (wrapper object)

--- javascript/index.js
+++ javascript/index.js
@@ -1,71 +1,146 @@
-/*     < 자바스크립트의 자료 표현 형식 > */
-
 /*
-- boolean, null, undefined, number, bigint, string, symbol 이상의 기본 자료형(Primitive : 원시 타입)과
-  참조형  Object로 구성되며 변수의 타입이 아님에 유의.   Object를 제외한 기본 자료형은 더 이상 쪼갤수
-  없는 값을 의미하고 이러한 값을 원시값(Primitive value)이라 지칭하며 설정 및 쓰기가 불가한 값(상수)을
-  의미.
+    < 래퍼 객체(Wrapper Object) >
 
+- number, string, boolean, symbol, bigint등의 원시 타입의 값을 감싸는 형태로 각각의 데이터
+  타입에 대응하는  Number, String, Boolean, Symbol , BigInt등의 객체가 제공되어  원시 값에
+  대한  체이닝(Chaining) 연산자(멤버연산자) .를 통한  속성(Property)이나  메서드에 접근 시
+  자동으로 생성되어 참조가 끝나면 소멸.
 
-boolean : null, NaN(Not a Number), Undefined, ""(빈문자열), 0 은  false와 같진 않지만  조건식이나 산술
-          연산 시 상호 변환하여 평가 가능하며, 그 외의 값에 대해서는 연산 시 true로 변환하여 평가 가능.
-          논리 연산자 !(not)을 이용하여 확인 가능.
+-----------------------------------------------------------------------------------------------
 
-undefined : 값을 할당하지 않은 변수(미 초기화)는 브라우저가 undefined값 할당.
-            또한 명시적 반환(return)을 하지않는 함수도 undefined값을 반환.
+    < Number >
 
-null : null은 사용자가 명시적으로 할당.
+- 래퍼(Wrapper) 객체로써 숫자 형태의 문자열 값을 숫자 타입으로 변환한 참조 및 값 반환.
 
-bigint : number보다 더 큰 정수를 표현하기 위한 타입. 숫자 상수에 접미사 n을 붙여 표현.
+형식 : new Number( 숫자 형태의 문자열 ) - 인수를 Number 타입의 참조형 객체로 저장 후 참조를
+                                          반환하되,  참조에 대한 연산을 적용 시 자동 언박싱
+                                          (Unboxing)을 통한 원시 숫자 타입으로 변환되어 처리.
 
-symbol : ECMAScript 6에서 추가된 타입으로 열거형과 유사한 고유한 식별자로써의 기능을 담당하며 객체의
-         속성 key값으로도 사용 가능.    다른 래퍼(Wrapper) 객체와는 달리 new연산자를 통한 Symbol객체
-         생성은 불가.
+       Number( 숫자 형태의 문자열 )     - 함수 호출 형태로써 변환된 원시 숫자 값을 직접 반환.
 
----------------------------------------------------------------------------------------------------------
+- 객체 생성 및 함수 호출 형식의 인수에 숫자 형태가 아닌 문자열을 전달하면 NaN(Not-a-Number)
+  반환.
+
 */
-
-// < 변수의 자료형 >
 
 'use strict';
 
-let value;
-
-value = 15;
-value = null;
-value = false;
-value = 'string';
-/* 자바스크립트에서 변수의 타입은 느슨한 타입(loosely typed) 또는 동적 타입(dynamic typed)이한
-      적용되어 별도의 변수의 타입을 구분하지 않으며 대입되는 값에 의해 그 타입이 동적으로 결정
-   따라서 하나의 변수에 여러 타입의 값을 저장 가능 */
-
-console.log(value);
+/* charAt : 인수로 문자열에 대한 인덱스를 지정함으로써 해당 위치의 문자값 반환 */
+console.log('string'.charAt(5));                // g
+/* 원시 값에 대한 속성 또는 메서드로 접근시 자동으로 래퍼 객체 생성함 */
 console.log();
 
 
-console.log(!null);
-console.log(!undefined);
-console.log(!NaN);
-console.log(!'');
-console.log(!0);
-/* boolean 타입의 값이 대상이 되는 논리연산자(!)에 다른 타입의 값을 대상으로 함으로써 자동으로
-      boolean 타입의 값으로 변환하여 연산 */
+const n1 = new Number('5');
+const n2 = Number('5');
+
+console.log(n1);                                // [Number: 5]
+/* Number객체의 참조 반환   =  [Number: 5] 반환 */
+console.log(n2);                                // 5
+/* 함수 호출을 통한 형변환된 값을 직접 반환   =  5 반환 */
+
+console.log(n1 + 5);                            // 10
+console.log(n2 + 5);                            // 10
+/* Number객체(n1)에 대한 산술 연산을 적용하여 자동 언박싱된 number로 변환되어 연산 처리 =  둘 다 10 반환 */
 console.log();
 
 
-let n;
+/* == 는 값만 비교 / === 는 type 까지 비교 */
+console.log(n1 == 5);                           // true
+console.log(n2 == 5);                           // true
+console.log(n1 === 5);                          // false
+console.log(n2 === 5);                          // true
 
-console.log(n);
-// n은 명시적 선언을 하였지만, 초기화하지 않았으므로 undefined값으로 평가
-console.log();
+/* typeof 연산자 : typeof 연산자 우변에 타입 측정 대상을 명시함으로써 문자열 형태로 타입 정보를 반환 */
+console.log(typeof n1);                         // object
+console.log(typeof n2);                         // number
 
-function test1() {
-      let n = 5;
+/* typeof 연산자를 활용한 타입체크 */
+if (typeof n1 === 'object') {                   // 타입 일치
+      console.log('타입 일치');
+} else {
+      console.log('타입 불일치');
 }
-function test2() {
-      return 10;
-}
-console.log(test1());
-// 명시적 반환을 하지 않는 함수도 undefined값을 반환
+console.log();
 
-console.log(test2());
+
+const n3 = new Number('a');
+const n4 = Number('a');
+
+console.log(n3);                                // [Number: NaN]
+/* 인수 값이 숫자 형태가 아닌 경우에는 NaN 저장 */
+console.log(n4);                                // NaN
+// 인수 값이 숫자 형태가 아닌 경우에는 NaN 반환
+console.log();
+
+
+/* 숫자 형태의 문자열에 대한 Number함수를 통한 숫자 타입으로 변환 연산. */
+const n5 = '8';
+const n6 = '10';
+
+console.log(n5 + n6);                           // 810
+/* 문자열끼리 결합했기 때문에 810 출력 */
+console.log(Number(n5) + n6);                   // 810
+/* n5는 number 타입으로 변환되었지만 문자열(n6)에 대한  + 연산자에 의해 결과적으로 문자열 결합 연산 적용 */
+console.log(Number(n5) + Number(n6));           // 18
+/* number 타입 결합으로 변환되었기 때문에 18 반환 */
+console.log();
+
+
+console.log(8 / 0);                             // Infinity
+/* Infinity 출력    =    양의 무한대 */
+console.log(-8 / 0);                            // -Infinity
+/* -Infinity 출력   =     NEGATIVE_INFINITY(음의 무한대) */
+console.log('string' / 0);                      // NaN
+console.log();
+
+
+/* < NaN의 특성과 타입 체크 시 주의점 > */
+console.log(NaN === NaN);                       // false
+/* NaN은 다른 NaN과 일치하지 않는 특성 */
+console.log(typeof NaN);                        // number
+/* NaN에 대한 직접적인 타입 체크 시 typeof 또한 연산자이므로 자동 number타입(0)으로 형변환됨에 주의 */
+console.log(typeof +'1234' === 'number');       // true
+console.log(typeof +'string' === 'number');     // true
+/* 문자열에 부호(+) 연산을 적용하였으므로 결과는 NaN으로 평가되지만 다시 typeof연산자를 적용함으로써 0으로 변환
+연산이 적용된 상태에서 타입평가를 함으로써 비교 연산의 결과는 true가 반환 */
+
+console.log(isNaN(NaN));                        // true
+/* isNaN : 인수의 대상이 NaN인지의 여부를 평가하여 boolean값 반환 */
+console.log();
+
+
+console.log(Number(undefined));                 // NaN
+/* undefined는 비교 연산 시 false로 평가되지만 number 형식으로 변환은 불가 */
+console.log(Number(null));                      // 0
+console.log(Number(true));                      // 1
+console.log(Number(false));                     // 0
+console.log(Number('  123  '));                 // 123
+/* 양쪽 끝의 공백을 제거한 후 변환된 number타입 값으로 반환 */
+console.log(Number('  12 3  '));                // NaN
+/* 양쪽 끝이 아닌 중간에 공백이나 숫자 형태가 아닌 값이 포함된 경우, 변환에 실패하여 NaN 반환 */
+console.log(Number('  '));                      // 0
+/* 공백이나 빈 문자열은 0으로 변환하여 반환 */
+console.log();
+
+
+let n7;
+let n8 = null;
+let n9 = true;
+
+console.log(n7 + 5);                            // NaN
+/* n7은 초기화되지 않았으므로 undefined로 평가되며, undefined는 연산 시 숫자형식으로 변환이 불가하므로 NaN 반환 */
+console.log(n8 + 5);                            // 5
+console.log(n9 + 5);                            // 6
+console.log(' ' + 5);                           // (공백)5
+/* 공백은 래퍼 객체를 통한 명시적 변환인 경우에는 0이 반환되지만, 이와 같이 연산 시에는 자동 형변환되지 않음 */
+console.log();
+
+
+const n10 = 4;
+const n11 = 10;
+
+console.log(String(n10) + String(n11));         // 410
+/* String 래퍼 객체를 통한 문자열 변환 */
+console.log(String(n7) + String(n8));           // undefinednull
+/* String 래퍼 객체를 통한 문자열 변환 */

e0c399460ac2f3cae7593fb386cfdd8da7732905
User-2023TXJQUV\Administrator <jin.harry97@gmail.com>
2023-07-07 10:45:20 +0900

    let, const

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,36 @@
 'use strict';
 
-/* 
-      < 사이트 URL을 이용한 비밀번호 생성 >
+console.log(n1);
+// 변수가 선언되지 않았으므로 컴파일 에러
 
-step1 : "http://" 부분은 제외하고 추출.                             =>      daum.net
-step2 : 닷(.)부터 표시되는 문자열을 제외하여 추출.                   =>      daum
-step3 : '$' + 처음 3자리 + 총 글자 수 + url 의 14번째 문자.         =>      $dau4e
+let n1 = 5;
+let n1 = 5;
+// var과 같이 중복 선언 불가.
 
-※ url 의 14번째 문자가 존재하지 않을 때는 ? 문자로 표시.            =>      $dau4?
-*/
+console.log(n1);
+console.log();
 
-let url = 'http://daum.net';
-let step1, step2, step3;
 
-step1 = url.split('/')[2];
+function letTest() {
+    let n2 = 10;
+// 함수 안에서 선언되었으므로 지역변수
+}
+console.log(n2);
+// let 이 지역변수이므로 n2에 대한 변수가 존재하지 않음 따라서 컴파일 에러
 
-step2 = step1.split('.')[0];
 
-step3 = `$${step2.slice(0, 3)}${step2.length}${url.length < 14 ? '?' : url[13]}`;
+{
+    let n3 = 15;
+// 블럭 안에서 선언되었으므로 지역변수
+}
+console.log(n3);
+// let 이 지역변수이므로 n3에 대한 변수가 존재하지 않음 따라서 컴파일 에러
+console.log();
 
-//  = `$${step2.slice(0, 3)}${step2.length}${url.charAt(13) ? url.charAt(13) : '?'}`;
-//  = `$${step2.slice(0, 3)}${step2.length}${url[13] ? url[13] : '?'}`;
 
-alert(step3);
+const n4 = 8;
+n4 = 9;
+// const : 변수에 대한 값부 변경 불가(immutable)하므로 컴파일 에러
+
+const n5;
+// const 선언이 지정된 변수는 반드시 선언과 동시에 초기화가 강제되고 선언 이후에 지연 초기화도 불가
